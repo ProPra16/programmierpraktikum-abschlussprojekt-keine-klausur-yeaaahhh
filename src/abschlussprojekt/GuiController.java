643448f1ac36b20b7f64d1d16a06df62c167607c
Serdar <serdar.yavuz@hhu.de>
2016-06-25 18:44:33 +0200

    anfangsGui

---
+++ src/abschlussprojekt/GuiController.java
@@ -0,0 +1,17 @@
+package abschlussprojekt;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GuiController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+    @FXML
+    void onWeiterclick(ActionEvent event) {
+    }
+}
