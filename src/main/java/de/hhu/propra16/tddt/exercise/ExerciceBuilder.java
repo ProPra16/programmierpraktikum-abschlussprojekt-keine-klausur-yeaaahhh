576ba66a30967e0b7d84b47131e60529ec45fefa
Felix Michels <felixm.michels@gmail.com>
2016-06-27 12:01:53 +0200

    Add default description and more exceptions to ExercieBuilder

--- src/main/java/de/hhu/propra16/tddt/exercise/ExerciceBuilder.java
+++ src/main/java/de/hhu/propra16/tddt/exercise/ExerciceBuilder.java
@@ -9,32 +9,37 @@ import vk.core.api.CompilationUnit;
 public class ExerciceBuilder {
 
     private final String name;
-    private String description;
+    private String description = "No description available";
     private String className;
     private String testName;
     private final SourceCodeBuilder sourceBuilder = new SourceCodeBuilder();
 
     public void setDescription(String description) {
+        if (description == null) throw new NullPointerException("Desctipion must not be null.");
         this.description = description;
     }
 
     public void setTestName(String testName) {
+        if (testName == null) throw new NullPointerException("Test name must not be null.");
         this.testName = testName;
     }
 
     public void setClassName(String className) {
+        if (className == null) throw new NullPointerException("Class name must not be null.");
         this.className = className;
     }
 
     public void setClassCode(String classCode) {
-        if (className == null) throw new IllegalArgumentException("Class name is not set.");
+        if (className == null) throw new IllegalStateException("Class name is not set.");
+        if (classCode == null) throw new NullPointerException("Class code must no be null.");
         CompilationUnit unit = new CompilationUnit(className, classCode, false);
         sourceBuilder.add(unit);
         className = null;
     }
 
     public void setTestCode(String testCode) {
-        if (testName == null) throw new IllegalArgumentException("Test name is not set.");
+        if (testName == null) throw new IllegalStateException("Test name is not set.");
+        if (testCode == null) throw new NullPointerException("Test code must no be null.");
         CompilationUnit unit = new CompilationUnit(className, testCode, true);
         sourceBuilder.add(unit);
         testName = null;
