949999fa9568a57105dc5a9ef28b5ae2655f8ba2
Felix Michels <felixm.michels@gmail.com>
2016-06-27 11:55:35 +0200

    Finish first draft of ExerciseBuilder

--- src/main/java/de/hhu/propra16/tddt/exercise/ExerciceBuilder.java
+++ src/main/java/de/hhu/propra16/tddt/exercise/ExerciceBuilder.java
@@ -1,5 +1,8 @@
 package de.hhu.propra16.tddt.exercise;
 
+import de.hhu.propra16.tddt.sourcecode.SourceCodeBuilder;
+import vk.core.api.CompilationUnit;
+
 /**
  * Created by felix on 6/27/16.
  */
@@ -9,8 +12,7 @@ public class ExerciceBuilder {
     private String description;
     private String className;
     private String testName;
-    private String classCode;
-    private String testCode;
+    private final SourceCodeBuilder sourceBuilder = new SourceCodeBuilder();
 
     public void setDescription(String description) {
         this.description = description;
@@ -25,11 +27,21 @@ public class ExerciceBuilder {
     }
 
     public void setClassCode(String classCode) {
-        this.classCode = classCode;
+        if (className == null) throw new IllegalArgumentException("Class name is not set.");
+        CompilationUnit unit = new CompilationUnit(className, classCode, false);
+        sourceBuilder.add(unit);
+        className = null;
     }
 
     public void setTestCode(String testCode) {
-        this.testCode = testCode;
+        if (testName == null) throw new IllegalArgumentException("Test name is not set.");
+        CompilationUnit unit = new CompilationUnit(className, testCode, true);
+        sourceBuilder.add(unit);
+        testName = null;
+    }
+
+    public Exercise build() {
+        return new Exercise(sourceBuilder.build(), name, description);
     }
 
 
