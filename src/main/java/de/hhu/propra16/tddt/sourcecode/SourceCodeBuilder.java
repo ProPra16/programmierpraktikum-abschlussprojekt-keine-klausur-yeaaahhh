86280a58682fcfe73140982bd8203f220c7ea0eb
Felix Michels <felixm.michels@gmail.com>
2016-06-27 11:17:09 +0200

    Add SourceCodeBuilder and package private SourceCode constructor

---
+++ src/main/java/de/hhu/propra16/tddt/sourcecode/SourceCodeBuilder.java
@@ -0,0 +1,22 @@
+package de.hhu.propra16.tddt.sourcecode;
+
+import vk.core.api.CompilationUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by felix on 6/26/16.
+ */
+public class SourceCodeBuilder {
+
+    private final List<CompilationUnit> units = new ArrayList<>();
+
+    public void add(CompilationUnit unit) {
+        units.add(unit);
+    }
+
+    public SourceCode build() {
+        return new SourceCode(units);
+    }
+}
