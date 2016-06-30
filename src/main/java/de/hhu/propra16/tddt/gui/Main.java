36414dbb112d977450bfc039c8b9c65ded5bf003
Bennett <Bennett.K@gmx.de>
2016-06-30 14:13:28 +0200

    set correct packagename in Gui

---
+++ src/main/java/de/hhu/propra16/tddt/gui/Main.java
@@ -0,0 +1,49 @@
+package de.hhu.propra16.tddt.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+	Stage redstage;
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage redStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resources/GuiRed.fxml"));     
+		Parent root = (Parent)fxmlLoader.load();          
+		GuiController controller = fxmlLoader.<GuiController>getController();
+		redStage.setTitle("TDDT");
+		redStage.setScene(new Scene(root,600, 600));
+		redStage.show();
+		
+		StageContainer greenSC = createStage("/resources/GuiGreen.fxml");
+		StageContainer refactorCodeSC = createStage("/resources/GuiRefactorCode.fxml");
+		StageContainer refactorTestSC = createStage("/resources/GuiRefactorTest.fxml");
+		
+		
+		greenSC.c.setStages(refactorCodeSC.s, redStage);
+		refactorCodeSC.c.setStages(refactorTestSC.s, greenSC.s);
+		refactorTestSC.c.setStages(redStage,refactorCodeSC.s);
+		controller.setStages(greenSC.s, null);
+
+	}
+	
+	public StageContainer createStage(String fxml) throws IOException{
+		Stage s = new Stage();
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxml));     
+		Parent root = (Parent)fxmlLoader.load();   
+		GuiController controller = fxmlLoader.<GuiController>getController();
+		s.setTitle("TDDT");
+		s.setScene(new Scene(root,600, 600));
+		StageContainer sc = new StageContainer(s,controller);
+		return sc;
+	}
+}
---
+++ src/main/java/de/hhu/propra16/tddt/gui/StageContainer.java
@@ -0,0 +1,14 @@
+package de.hhu.propra16.tddt.gui;
+
+import javafx.stage.Stage;
+
+public class StageContainer {
+
+	public Stage s;
+	public GuiController c;
+	
+	public StageContainer(Stage s, GuiController c){
+		this.s = s;
+		this.c = c;
+	}
+}
