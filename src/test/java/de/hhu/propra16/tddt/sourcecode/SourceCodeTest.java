6d880ee3d5ce68bb2032cf759e8914c7556e3724
Natalie Braam <natalie.braam@hhu.de>
2016-06-30 16:54:20 +0200

    SourceCode Tests

--- src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
+++ src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
@@ -1,16 +1,42 @@
 package de.hhu.propra16.tddt.sourcecode;
 
 import org.junit.Before;
+import org.junit.Test;
+import vk.core.api.CompilationUnit;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Natter on 30.06.16.
+ * Created by NatalieHHU on 30.06.16.
  */
 public class SourceCodeTest {
+    SourceCode SC;
     @Before
     public void setUp() throws Exception {
-
+        CompilationUnit a = new CompilationUnit("Test1", "public class blaTest {do some stuff}", true);
+        CompilationUnit b = new CompilationUnit("Test2", "public class bla{}", false);
+        CompilationUnit c = new CompilationUnit("Test3", "public blubbTest {do something}", true);
+        CompilationUnit d = new CompilationUnit("Test4", "public class blubb{}", false);
+        List<CompilationUnit> liste = new ArrayList<>();
+        liste.add(a);
+        liste.add(b);
+        liste.add(c);
+        liste.add(d);
+        SC = new SourceCode(liste);
     }
-
+    @Test
+    public void testgetStringCode() {
+        assertEquals("public class bla{}", SC.getStringCode("Test2"));
+    }
+    @Test
+    public void testgetNameTest() {
+        assertEquals("Test1", SC.getNameTest().get(0));
+    }
+    @Test
+    public void testgetNameCode() {
+        assertEquals("Test4", SC.getNameCode().get(1));
+    }
 }
