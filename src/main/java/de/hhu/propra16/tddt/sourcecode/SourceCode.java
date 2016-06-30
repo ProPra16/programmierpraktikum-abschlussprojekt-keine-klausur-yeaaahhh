33fa8ba08cdbf1f0b2ec59d84a0aaf6c88e3580b
Natalie Braam <natalie.braam@hhu.de>
2016-06-30 15:51:11 +0200

    SourceCode changes/ add methods

--- src/main/java/de/hhu/propra16/tddt/sourcecode/SourceCode.java
+++ src/main/java/de/hhu/propra16/tddt/sourcecode/SourceCode.java
@@ -2,7 +2,6 @@ package de.hhu.propra16.tddt.sourcecode;
 
 import vk.core.api.CompilationUnit;
 import vk.core.api.CompilerFactory;
-import vk.core.api.CompilerResult;
 import vk.core.internal.InternalCompiler;
 import vk.core.internal.InternalResult;
 
@@ -24,8 +23,8 @@ public class SourceCode {
     }
 
     // Methode splittet units in ArrayList aus Tests und Codes
-    public void split() {
-        units.forEach((element)-> {
+    private void split() {
+        units.forEach((element) -> {
             if (element.isATest())
                 tests.add(element);
             else
@@ -33,11 +32,10 @@ public class SourceCode {
         });
     }
 
-    public void compileTest() {
-        split();
+    public void compile() {
         List<InternalResult> result = new ArrayList<>();
-        synchronized (tests) {
-            tests.forEach((element) -> {
+        synchronized (units) {
+            units.forEach((element) -> {
                 InternalCompiler IC = (InternalCompiler) CompilerFactory.getCompiler(element);
                 IC.compileAndRunTests();
                 result.add((InternalResult) IC.getTestResult());
@@ -45,32 +43,57 @@ public class SourceCode {
         }
     }
 
-    public void compileCode() {
-        split();
-        List<InternalResult> result = new ArrayList<>();
-        synchronized (code) {
-            code.forEach((element) -> {
-                InternalCompiler IC = (InternalCompiler) CompilerFactory.getCompiler(element);
-                IC.compileAndRunTests();
-                result.add((InternalResult) IC.getCompilerResult());
-            });
+    // Methode bekommt einen Klassennamen und gibt den SourceCode als String zurück
+    public String getStringCode(String Klassenname) {
+        for (int i=0; i<units.size(); i++) {
+            if (units.get(i).getClassName().equals(Klassenname))
+                return units.get(i).getClassContent();
         }
+       return "";
     }
+
+    //Methode gibt Klassenname der Tests aus
+    public List<String> getNameTest() {
+        split();
+        List<String> stringTest = new ArrayList<>();
+        tests.forEach((t) -> {
+            stringTest.add(t.getClassName());
+        });
+        return stringTest;
+    }
+
+    //Methode gibt Klassenname der Programme aus
+    public List<String> getNameCode() {
+        split();
+        List<String> stringCode = new ArrayList<>();
+        code.forEach((c) -> {
+            stringCode.add(c.getClassName());
+        });
+        return stringCode;
+    }
+
 
     // Methode wird vom Trainer aufgerufen; Trainer weiß in welcher Phase wir uns befinden
     // className: getClassName() , String aus Textfield für Content, boolean je nach Phase
-    public void addCompilationUnit(CompilationUnit cu) {
-        units.add(cu);
-    }
+    //public void addCompilationUnit(CompilationUnit cu) {units.add(cu);}
 
     public static void main(String[] args) {
         CompilationUnit a = new CompilationUnit("Beispiel", "public class bla{static void blaa{Beispiel2.do(2)}}", true);
         CompilationUnit b = new CompilationUnit("Beispiel2", "public class blubb{ static void blub{}} ", false);
+        CompilationUnit c = new CompilationUnit("bsp", "public blaaa ", true);
         List<CompilationUnit> liste = new ArrayList<>();
         liste.add(a);
         liste.add(b);
+        liste.add(c);
         SourceCode sc = new SourceCode(liste);
-      sc.compileTest();
+      sc.compile();
+        List<String> bb = new ArrayList<>();
+        bb = sc.getNameCode();
+        for (int i = 0; i<bb.size(); i++)
+            System.out.println(bb.get(i));
+
+
+        //System.out.println(sc.getStringCode("Beispiel2"));
     }
 
 }
---
+++ src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
@@ -0,0 +1,16 @@
+package de.hhu.propra16.tddt.sourcecode;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Natter on 30.06.16.
+ */
+public class SourceCodeTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
