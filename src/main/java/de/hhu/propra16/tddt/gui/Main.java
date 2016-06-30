3ba602d5db95ff4a616cb9737b1e5e1d0b9a95ba
Bennett <Bennett.K@gmx.de>
2016-06-30 14:43:14 +0200

    set correct packagename to Gui

--- src/main/java/de/hhu/propra16/tddt/gui/Main.java
+++ src/main/java/de/hhu/propra16/tddt/gui/Main.java
@@ -1,13 +1,12 @@
-package Gui;
-
-import java.io.IOException;
-
+package de.hhu.propra16.tddt.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
