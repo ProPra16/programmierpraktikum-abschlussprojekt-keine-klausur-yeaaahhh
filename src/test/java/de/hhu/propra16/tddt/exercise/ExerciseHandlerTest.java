4408e8f2f825150f5c6d868bde6ff9da0f3587c7
Bennett <Bennett.K@gmx.de>
2016-07-01 15:06:32 +0200

    rearrange @Before and add Test, if setName(null) is called

--- src/test/java/de/hhu/propra16/tddt/exercise/ExerciseHandlerTest.java
+++ src/test/java/de/hhu/propra16/tddt/exercise/ExerciseHandlerTest.java
@@ -12,37 +12,7 @@ public class ExerciseHandlerTest {
     private Attributes exerciseAtts;
 
     @Before
-    public void setupTest1() {
-        builder = mock(ExerciseBuilder.class);
-        handler = new ExerciseHandler(() -> builder);
-    }
-
-    @Test
-    public void test_startElement_withExercises() {
-        handler.startElement("", "", "exercises", null);
-        verify(builder, times(0)).setName("Name");
-    }
-
-    @Before
-    public void setupTest2() {
-        builder = mock(ExerciseBuilder.class);
-        handler = new ExerciseHandler(() -> builder);
-    }
-
-    @Test
-    public void test_startElement_withExercise() {
-        Attributes atts = mock(Attributes.class);
-        when(atts.getLocalName(0)).thenReturn("name");
-        when(atts.getValue(0)).thenReturn("Test");
-        when(atts.getLength()).thenReturn(1);
-
-        handler.startElement("", "", "exercises", null);
-        handler.startElement("", "", "exercise", atts);
-        verify(builder, times(1)).setName("Test");
-    }
-
-    @Before
-    public void setupEnd() {
+    public void setup() {
         builder = mock(ExerciseBuilder.class);
         handler = new ExerciseHandler(() -> builder);
 
@@ -50,14 +20,33 @@ public class ExerciseHandlerTest {
         when(exerciseAtts.getLocalName(0)).thenReturn("name");
         when(exerciseAtts.getValue(0)).thenReturn("Test");
         when(exerciseAtts.getLength()).thenReturn(1);
+
+        handler.startElement("", "", "exercises", null);
+    }
+
+    @Test
+    public void test_startElement_withExercises() {
+        verify(builder, times(0)).setName("Name");
+    }
+
+    @Test
+    public void test_startElement_withExercise_withAttsGreater0() {
+        handler.startElement("", "", "exercise", exerciseAtts);
+        verify(builder, times(1)).setName("Test");
+    }
+
+    @Test
+    public void test_startElement_withExercise_withAttsEquals0() {
+        exerciseAtts = mock(Attributes.class);
+        handler.startElement("", "", "exercise", exerciseAtts);
+        verify(builder, times(1)).setName(null);
     }
 
     @Test
     public void test_characters() {
         char[] sequence = {'H', 'a', 'l', 'l', 'o'};
-        handler.characters(sequence, 0, 5);
-        handler.startElement("", "", "exercises", null);
         handler.startElement("", "", "exercise", exerciseAtts);
+        handler.characters(sequence, 0, 5);
         handler.endElement("", "", "description");
         verify(builder, times(1)).setDescription("Hallo");
     }
