610dce14f25cbf3f04a5255fc1d3175ec26a910c
Bennett <Bennett.K@gmx.de>
2016-07-01 16:45:19 +0200

    add Tests

--- src/test/java/de/hhu/propra16/tddt/exercise/ExerciseHandlerTest.java
+++ src/test/java/de/hhu/propra16/tddt/exercise/ExerciseHandlerTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.Attributes;
 
+import java.time.Duration;
+
 import static org.mockito.Mockito.*;
 
 public class ExerciseHandlerTest {
@@ -25,29 +27,85 @@ public class ExerciseHandlerTest {
     }
 
     @Test
-    public void test_startElement_withExercises() {
-        verify(builder, times(0)).setName("Name");
+    public void start_withExercises() {
+        verify(builder, times(0)).setName("Test");
     }
 
     @Test
-    public void test_startElement_withExercise_withAttsGreater0() {
+    public void start_withExercise_withAttsGreater0() {
         handler.startElement("", "", "exercise", exerciseAtts);
         verify(builder, times(1)).setName("Test");
     }
 
     @Test
-    public void test_startElement_withExercise_withAttsEquals0() {
+    public void start_withExercise_withAttsEquals0() {
         exerciseAtts = mock(Attributes.class);
         handler.startElement("", "", "exercise", exerciseAtts);
         verify(builder, times(1)).setName(null);
     }
 
     @Test
-    public void test_characters() {
+    public void start_withExercise_withClassName() {
+        handler.startElement("", "", "exercise", exerciseAtts);
+        handler.startElement("", "", "class", exerciseAtts);
+        verify(builder, times(1)).setClassName("Test");
+    }
+
+    @Test
+    public void start_withExercise_withTestName() {
+        handler.startElement("", "", "exercise", exerciseAtts);
+        handler.startElement("", "", "test", exerciseAtts);
+        verify(builder, times(1)).setTestName("Test");
+    }
+
+    @Test
+    public void start_withExercise_withBabysteps() {
+        exerciseAtts = mock(Attributes.class);
+        when(exerciseAtts.getLength()).thenReturn(2);
+        when(exerciseAtts.getLocalName(0)).thenReturn("value");
+        when(exerciseAtts.getLocalName(1)).thenReturn("time");
+        when(exerciseAtts.getValue(0)).thenReturn("true");
+        when(exerciseAtts.getValue(1)).thenReturn("5:00");
+
+        handler.startElement("", "", "exercise", exerciseAtts);
+        handler.startElement("", "", "babysteps", exerciseAtts);
+        verify(builder, times(1)).setBabySteps(true);
+        verify(builder, times(1)).setTime(Duration.parse("PT5M0S"));
+    }
+
+    @Test
+    public void characters_regular() {
         char[] sequence = {'H', 'a', 'l', 'l', 'o'};
         handler.startElement("", "", "exercise", exerciseAtts);
         handler.characters(sequence, 0, 5);
         handler.endElement("", "", "description");
         verify(builder, times(1)).setDescription("Hallo");
+    }
+
+    @Test
+    public void characters_withSpaces() {
+        char[] sequence = {' ', ' ', 'l', ' ', 'o'};
+        handler.startElement("", "", "exercise", exerciseAtts);
+        handler.characters(sequence, 0, 5);
+        handler.endElement("", "", "description");
+        verify(builder, times(1)).setDescription("l o");
+    }
+
+    @Test
+    public void characters_onlyWhitespace() {
+        char[] sequence = {' ', '\n', ' ', '\n', '\n'};
+        handler.startElement("", "", "exercise", exerciseAtts);
+        handler.characters(sequence, 0, 5);
+        handler.endElement("", "", "description");
+        verify(builder, times(1)).setDescription("");
+    }
+
+    @Test
+    public void characters_withSpacesAndNewLines() {
+        char[] sequence = {'\n', ' ', ' ', '\n', 'o'};
+        handler.startElement("", "", "exercise", exerciseAtts);
+        handler.characters(sequence, 0, 5);
+        handler.endElement("", "", "description");
+        verify(builder, times(1)).setDescription("o");
     }
 }
