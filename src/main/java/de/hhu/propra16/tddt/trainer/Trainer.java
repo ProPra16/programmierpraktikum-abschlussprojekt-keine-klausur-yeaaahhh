1c2a67141972fc3ae415bba35d0c246771be77ac
Bennett <Bennett.K@gmx.de>
2016-07-05 14:14:31 +0200

    delete unneeded Code

--- src/main/java/de/hhu/propra16/tddt/trainer/Trainer.java
+++ src/main/java/de/hhu/propra16/tddt/trainer/Trainer.java
@@ -2,55 +2,13 @@ package de.hhu.propra16.tddt.trainer;
 
 import de.hhu.propra16.tddt.exercise.Exercise;
 import de.hhu.propra16.tddt.sourcecode.SourceCode;
+import de.hhu.propra16.tddt.sourcecode.SourceCodeBuilder;
 
 
 /**
  * Created by tim on 30.06.2016.
  */
 public class Trainer{
-    /*// bekomme Sourcecode von Interface
-    private boolean nextPhaseaccepted;
-    Exercise excersise;
-    CheckCompile checkcompile;
-    Editor editor;
-    Timer timer = new Timer();
-    SourceCode previousSorcecode = excersise.getSources();
-
-    public Trainer(Exercise excersise, CheckCompile checkcompile, Editor editor){
-        this.excersise = excersise;
-        this.checkcompile = checkcompile;
-        this.editor = editor;
-    }
-    public boolean checkPhaseRed(){
-        nextPhaseaccepted = false;
-        if (checkcompile.check(editor.get(), Phase.RED) == false){
-            previousSorcecode = editor.get();
-            nextPhaseaccepted = true;
-        }
-        return nextPhaseaccepted;
-    }
-
-    public boolean checkPhaseGreen(){
-        nextPhaseaccepted = false;
-        if (checkcompile.check(editor.get(), Phase.GREEN) == true){
-            previousSorcecode = editor.get();
-            nextPhaseaccepted = true;
-        }
-        return nextPhaseaccepted;
-    }
-
-    public boolean checkPhaseRefactor(){
-        nextPhaseaccepted = false;
-        if (checkcompile.check(editor.get(), Phase.BLACK) == true){
-            previousSorcecode = editor.get();
-            nextPhaseaccepted = true;
-        }
-        return nextPhaseaccepted;
-    }
-    public void returntoPhaseRED(){
-        editor.show(previousSorcecode);
-    }
-    */
     private Exercise exercise;
     private Phase phase;
     private CheckCompile checker;
@@ -66,11 +24,14 @@ public class Trainer{
     public void nextPhase() {
         if (checker.check(exercise.getSources(), phase)) {
             cycle(true);
+            previous = current;
+            SourceCodeBuilder scBuilder = new SourceCodeBuilder();
         }
     }
 
     public void previousPhase() {
         cycle(false);
+        current = previous;
     }
 
     private void cycle(boolean forward) {
