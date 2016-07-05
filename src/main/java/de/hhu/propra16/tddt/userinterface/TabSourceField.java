1bf1312e58e3bc9459d13095ed1d981ed89ffcc6
Felix Michels <felixm.michels@gmail.com>
2016-07-05 21:05:24 +0200

    Implement showText in TabSourceField

--- src/main/java/de/hhu/propra16/tddt/userinterface/TabSourceField.java
+++ src/main/java/de/hhu/propra16/tddt/userinterface/TabSourceField.java
@@ -1,12 +1,17 @@
 package de.hhu.propra16.tddt.userinterface;
 
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+
+import java.util.Map;
 
 /**
  * Created by felix on 7/5/16.
  */
 public class TabSourceField extends TabPane implements SourceField {
 
+    // Every value in codeAreas must be a child!
+    private Map<String, TextArea> codeAreas;
 
     @Override
     public void setEditable(boolean editable) {
@@ -14,7 +19,12 @@ public class TabSourceField extends TabPane implements SourceField {
 
     @Override
     public void showText(String name, String text) {
-
+        if (!codeAreas.keySet().contains(name)) {
+            TextArea area = new TextArea();
+            codeAreas.put(name, area);
+            getChildren().add(area);
+        }
+        codeAreas.get(name).setText(text);
     }
 
     @Override
