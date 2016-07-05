338a18eb641e5590c4b2439ce8cede67c75aa29a
Felix Michels <felixm.michels@gmail.com>
2016-07-05 20:58:44 +0200

    Create PaneSourceField skeleton

---
+++ src/main/java/de/hhu/propra16/tddt/userinterface/TabSourceField.java
@@ -0,0 +1,24 @@
+package de.hhu.propra16.tddt.userinterface;
+
+import javafx.scene.control.TabPane;
+
+/**
+ * Created by felix on 7/5/16.
+ */
+public class TabSourceField extends TabPane implements SourceField {
+
+
+    @Override
+    public void setEditable(boolean editable) {
+    }
+
+    @Override
+    public void showText(String name, String text) {
+
+    }
+
+    @Override
+    public String getTextOf(String name) {
+        return null;
+    }
+}
