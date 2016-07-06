bec092e0781b77e292317db4f76144ac37858602
Felix Michels <felixm.michels@gmail.com>
2016-07-06 17:27:01 +0200

    Clean up SourceCode tests

--- src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
+++ src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
@@ -13,47 +13,43 @@ import static org.junit.Assert.*;
  * Created by NatalieHHU on 30.06.16.
  */
 public class SourceCodeTest {
-    SourceCode SC1;
-    SourceCode SC2;
+    SourceCode sc1;
+    SourceCode sc2;
+
     @Before
     public void setUp() throws Exception {
         CompilationUnit a = new CompilationUnit("Test1", "import org.junit.*; public class Test1{ @Test public void dat(){bla.doSomething();}}", true);
         CompilationUnit b = new CompilationUnit("bla", "public class bla{}", false);
         CompilationUnit c = new CompilationUnit("Test3", "import org.junit.*; public Test22 {@Test public void dot(){}}", true);
         CompilationUnit d = new CompilationUnit("blubb", "public class blubb{int i =1}", false);
-        List<CompilationUnit> liste1 = new ArrayList<>();
-        List<CompilationUnit> liste2 = new ArrayList<>();
-        liste1.add(a);
-        liste1.add(b);
-        liste2.add(c);
-        liste2.add(d);
-        SC1 = new SourceCode(liste1);
-        SC2 = new SourceCode(liste2);
+
+        sc1 = new SourceCodeBuilder().add(a).add(b).build();
+        sc2 = new SourceCodeBuilder().add(c).add(d).build();
     }
     @Test
     public void testgetStringCode() {
-        assertEquals("public class bla{}", SC1.getStringCode("bla"));
+        assertEquals("public class bla{}", sc1.getStringCode("bla"));
     }
 
     @Test
     public void testgetNameTest() {
-        assertEquals("Test1", SC1.getNameTest().get(0));
+        assertEquals("Test1", sc1.getNameTest().get(0));
     }
 
     @Test
     public void testgetNameCode() {
-        assertEquals("blubb", SC2.getNameCode().get(0));
+        assertEquals("blubb", sc2.getNameCode().get(0));
     }
 
     @Test
     public void FailedTest() {
-        System.out.println(SC1.getResult());
-//        assertEquals(1,SC1.numberOfFailedTests());
+        System.out.println(sc1.getResult());
+//        assertEquals(1,sc1.numberOfFailedTests());
     }
 
     @Test
     public void CodeError() {
-        System.out.println(SC2.getResult());
+        System.out.println(sc2.getResult());
 
     }
 }
