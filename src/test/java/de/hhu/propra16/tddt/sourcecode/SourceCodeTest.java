6df89686f16bbf137cad3c2cb2bea76c64445303
Felix Michels <felixm.michels@gmail.com>
2016-07-06 17:50:11 +0200

    Add a SourceCode test

--- src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
+++ src/test/java/de/hhu/propra16/tddt/sourcecode/SourceCodeTest.java
@@ -4,27 +4,30 @@ import org.junit.Before;
 import org.junit.Test;
 import vk.core.api.CompilationUnit;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by NatalieHHU on 30.06.16.
  */
 public class SourceCodeTest {
-    SourceCode sc1;
-    SourceCode sc2;
+    private SourceCode sc1;
+    private SourceCode sc2;
+    private SourceCode sc3;
 
     @Before
     public void setUp() throws Exception {
-        CompilationUnit a = new CompilationUnit("Test1", "import org.junit.*; public class Test1{ @Test public void dat(){bla.doSomething();}}", true);
+        CompilationUnit a = new CompilationUnit("Test1", "import org.junit.*; public class Test1{ @Test public void dat(){ Assert.assertTrue(false);}}", true);
         CompilationUnit b = new CompilationUnit("bla", "public class bla{}", false);
-        CompilationUnit c = new CompilationUnit("Test3", "import org.junit.*; public Test22 {@Test public void dot(){}}", true);
+        CompilationUnit c = new CompilationUnit("Test3", "import org.junit.*; public class Test3 {@Test public void dot(){}}", true);
         CompilationUnit d = new CompilationUnit("blubb", "public class blubb{int i =1}", false);
 
         sc1 = new SourceCodeBuilder().add(a).add(b).build();
         sc2 = new SourceCodeBuilder().add(c).add(d).build();
+        sc3 = new SourceCodeBuilder().add(b).add(c).build();
     }
     @Test
     public void testgetStringCode() {
@@ -44,12 +47,20 @@ public class SourceCodeTest {
     @Test
     public void FailedTest() {
         System.out.println(sc1.getResult());
-//        assertEquals(1,sc1.numberOfFailedTests());
+        assertFalse(sc1.hasCompileErrors());
+        assertEquals(1,sc1.numberOfFailedTests());
     }
 
     @Test
     public void CodeError() {
         System.out.println(sc2.getResult());
+        assertTrue(sc2.hasCompileErrors());
+        assertEquals(0, sc2.numberOfFailedTests());
+    }
 
+    @Test
+    public void everythingFine() {
+        assertEquals(0, sc3.numberOfFailedTests());
+        assertFalse(sc3.hasCompileErrors());
     }
 }
