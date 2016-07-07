5ae58179202de15061eaec4720d3a7b4f189fc00
Serdar <serdar.yavuz@hhu.de>
2016-07-07 14:47:27 +0200

    new Gui new style!

--- src/main/java/de/hhu/propra16/tddt/gui/GuiController.java
+++ src/main/java/de/hhu/propra16/tddt/gui/GuiController.java
@@ -1,47 +1,110 @@
 package de.hhu.propra16.tddt.gui;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GuiController implements Initializable  {
-	
-	
-	public Stage nextStage, previousStage;
-	
-	@Override
+public class GuiController implements Initializable {
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private Parent root;
+
+	@FXML
+	private URL location;
+
+	@FXML
+	void GuiController() {
+	}
+
+	@FXML
 	public void initialize(URL location, ResourceBundle resources) {
+
+
 	}
-	
-	public void setStages(Stage nextStage, Stage previousStage){
-		this.nextStage = nextStage;
-		this.previousStage = previousStage;
-	}
-	
 	@FXML
-	void next(ActionEvent event) {
+	void addTab(Event event) {
+		Tab t = (Tab) event.getSource();
+		TabPane tabs = t.getTabPane();
+		final Tab tab = new Tab("Tab " + (tabs.getTabs().size() + 1));
+		tabs.getTabs().add(tab);
+		tabs.getSelectionModel().select(tab);
+		tabs.getTabs().remove(tabs.getTabs().size()-2, tabs.getTabs().size()-1);
+		tabs.getTabs().add(t);
+		TextArea ta = new TextArea();
+		ta.setPrefSize(218, 469);
+
+		Pane p = new Pane();
+		p.setPrefSize(285,520);
+		p.getChildren().add(ta);
+
+		tab.setContent(p);
+
+
+	}
+
+	@FXML
+	void loadExcersise(ActionEvent event) {
+		/// Ladeprozedur der Uebung hier
+		try {
+			Button b = (Button) event.getSource();
+			Stage s = (Stage) b.getScene().getWindow();
+			Pane myPane = null;
+			myPane = FXMLLoader.load(getClass().getResource("/Scene.fxml"));
+			Scene scene = new Scene(myPane);
+			s.setScene(scene);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+	@FXML
+	void nextPhase(ActionEvent event) {
 		Button b = (Button) event.getSource();
 		Stage s = (Stage) b.getScene().getWindow();
-		nextStage.show();
-		TextArea t =(TextArea) nextStage.getScene().lookup("#useredit");
-		t.clear();
-		s.hide();
+		Text text = (Text) s.getScene().lookup("#phaseid");
+		TextArea left = (TextArea) s.getScene().lookup("#leftText");
+		TabPane right = (TabPane) s.getScene().lookup("#codeTabs");
+		if(text.getText().contains("Red Phase")){
+			left.setDisable(true);
+			right.setDisable(false);
+			text.setText("Green Phase");
+			text.setFill(Color.GREEN);
+		}else if(text.getText().contains("Refactor Phase")){
+			right.setDisable(true);
+			text.setText("Red Phase");
+			text.setFill(Color.RED);
+		}else if(text.getText().contains("Green Phase")){
+			left.setDisable(false);
+			text.setText("Refactor Phase");
+			text.setFill(Color.BLACK);
+		}
+
+
 	}
-	
-	@FXML
-	void previous(ActionEvent event) {
-	
-		Button b = (Button) event.getSource();
-		Stage s = (Stage) b.getScene().getWindow();
-        previousStage.show();
-        s.hide();
-    }
+
 
 
 }
+
+
+
+
--- src/main/java/de/hhu/propra16/tddt/gui/Main.java
+++ src/main/java/de/hhu/propra16/tddt/gui/Main.java
@@ -16,32 +16,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage redStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GuiRed.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Start.fxml"));
         Parent root = loader.load();
         GuiController controller = loader.getController();
         redStage.setTitle("TDDT");
-		redStage.setScene(new Scene(root,600, 600));
-		redStage.show();
+        redStage.setScene(new Scene(root, 800, 600));
+        redStage.show();
 
-        StageContainer greenSC = createStage("/GuiGreen.fxml");
-        StageContainer refactorCodeSC = createStage("/GuiRefactorCode.fxml");
-        StageContainer refactorTestSC = createStage("/GuiRefactorTest.fxml");
-
-
-        greenSC.c.setStages(refactorCodeSC.s, redStage);
-		refactorCodeSC.c.setStages(refactorTestSC.s, greenSC.s);
-		refactorTestSC.c.setStages(redStage,refactorCodeSC.s);
-		controller.setStages(greenSC.s, null);
-
-	}
-	
-	public StageContainer createStage(String fxml) throws IOException{
-		Stage s = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxml));
-        Parent root = fxmlLoader.load();
-        GuiController controller = fxmlLoader.getController();
-        s.setTitle("TDDT");
-        s.setScene(new Scene(root,600, 600));
-        return new StageContainer(s, controller);
     }
+
 }
