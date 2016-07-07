fd7e133fe9534eef9bc813532cffd31f5bc3ff09
Felix Michels <felixm.michels@gmail.com>
2016-07-07 21:46:01 +0200

    Refactor showText method in TabCodeField

--- src/main/java/de/hhu/propra16/tddt/userinterface/TabCodeField.java
+++ src/main/java/de/hhu/propra16/tddt/userinterface/TabCodeField.java
@@ -9,9 +9,6 @@ import javafx.scene.control.TextArea;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by felix on 7/5/16.
- */
 public class TabCodeField extends TabPane implements CodeField {
 
     // Every value in codeAreas must be a child in a tab of this!
@@ -34,15 +31,23 @@ public class TabCodeField extends TabPane implements CodeField {
     public void showText(String name, String text) {
         TextArea area = codeAreas.get(name);
         if (area == null) {
-            area = new TextArea();
-            area.setEditable(editable);
-            area.textProperty().addListener(e -> editNumber.setValue(editNumber.getValue() +1 ));
-            codeAreas.put(name, area);
-            Tab tab = new Tab(name);
-            tab.setContent(area);
-            getTabs().add(tab);
+            area = addArea(name);
         }
         area.setText(text);
+    }
+
+    private TextArea addArea(String name) {
+        // Set TextArea attributes
+        TextArea area = new TextArea();
+        area.setEditable(editable);
+        area.textProperty().addListener(e -> editNumber.setValue(editNumber.getValue() +1 ));
+
+        // Add it to a tab and the tab to this
+        codeAreas.put(name, area); // Very important, or the area won't be found in showText and getTextOf
+        Tab tab = new Tab(name);
+        tab.setContent(area);
+        getTabs().add(tab);
+        return area;
     }
 
     @Override
