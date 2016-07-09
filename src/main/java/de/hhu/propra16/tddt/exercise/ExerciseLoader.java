5d7b398e6fc0ff3ffc9c485ade42648d2debb02d
Serdar <serdar.yavuz@hhu.de>
2016-07-09 19:48:33 +0200

    ExerciseLoader update

--- src/main/java/de/hhu/propra16/tddt/exercise/ExerciseLoader.java
+++ src/main/java/de/hhu/propra16/tddt/exercise/ExerciseLoader.java
@@ -12,11 +12,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 public class ExerciseLoader {
 
 
-    public static List<Exercise> loadFile(InputSource is) {
+    private static List<Exercise> list;
+    public ObservableList ExTitles;
+    public ObservableList ExDescriptions;
+
+
+    public ExerciseLoader(InputSource is){
+        this.list = loadFile(is);
+    }
+
+    public List<Exercise> loadFile(InputSource is) {
         List<Exercise> list = new ArrayList<>();
         try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
@@ -28,12 +39,24 @@ public class ExerciseLoader {
         } catch (ParserConfigurationException | SAXException | IOException e) {
             System.out.println("Input has to be in correct format!");
         }
+
+        this.ExTitles = FXCollections.observableArrayList();
+        this.ExDescriptions = FXCollections.observableArrayList();
+        for (Exercise i: list) {
+            ExTitles.add(i.getName());
+            ExDescriptions.add(i.getDescription());
+        }
+
         return list;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+
+
+
+    }
+  /*  public static void main(String[] args) throws FileNotFoundException {
         Path p = Paths.get("katalog/Exercise.xml");
         InputSource in = new InputSource(p.toString());
         loadFile(in);
-    }
-}
+    }*/
+
