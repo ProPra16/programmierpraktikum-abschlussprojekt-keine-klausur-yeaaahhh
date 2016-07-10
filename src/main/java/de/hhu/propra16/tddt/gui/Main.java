2f0b8adc7ec41ec74e69fc6ce0a4464455351e62
Felix Michels <felixm.michels@gmail.com>
2016-07-10 18:50:11 +0200

    Fix merge corruption

--- src/main/java/de/hhu/propra16/tddt/gui/Main.java
+++ src/main/java/de/hhu/propra16/tddt/gui/Main.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-public class Main extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
