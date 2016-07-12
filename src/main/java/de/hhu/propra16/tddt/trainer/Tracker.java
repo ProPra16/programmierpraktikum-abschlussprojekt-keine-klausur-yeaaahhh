be6967749589e3819f7f29d5d3dcd5311e7214f1
Felix Michels <felixm.michels@gmail.com>
2016-07-12 13:49:33 +0200

    Tracker now stores the stuff it gets

--- src/main/java/de/hhu/propra16/tddt/trainer/Tracker.java
+++ src/main/java/de/hhu/propra16/tddt/trainer/Tracker.java
@@ -3,10 +3,28 @@ package de.hhu.propra16.tddt.trainer;
 import de.hhu.propra16.tddt.sourcecode.SourceCode;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tracker {
 
+    private static class TrackedObject {
+        private final SourceCode code;
+        private final Duration timeUsed;
+        private final Phase oldPhase;
+        private final Phase newPhase;
+
+        private TrackedObject(SourceCode code, Duration timeUsed, Phase oldPhase, Phase newPhase) {
+            this.code = code;
+            this.timeUsed = timeUsed;
+            this.oldPhase = oldPhase;
+            this.newPhase = newPhase;
+        }
+    }
+
+    private final List<TrackedObject> datapoints = new ArrayList<>();
+
     public void push(SourceCode code, Duration timeUsed, Phase oldPhase, Phase newPhase) {
-        
+        datapoints.add(new TrackedObject(code, timeUsed, oldPhase, newPhase));
     }
 }
