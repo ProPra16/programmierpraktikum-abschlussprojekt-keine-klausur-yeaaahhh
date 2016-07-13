c0a103a79b4eee939e2c1d4985518f4d394e9b81
Felix Michels <felixm.michels@gmail.com>
2016-07-13 22:12:55 +0200

    Fix split method

--- src/main/java/de/hhu/propra16/tddt/sourcecode/SourceCodeComparator.java
+++ src/main/java/de/hhu/propra16/tddt/sourcecode/SourceCodeComparator.java
@@ -1,9 +1,6 @@
 package de.hhu.propra16.tddt.sourcecode;
 
-import javax.sound.midi.spi.SoundbankReader;
-import javax.xml.transform.Source;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -11,27 +8,28 @@ import java.util.List;
  * Created by Natter on 07.07.16.
  */
 public class SourceCodeComparator {
-    private static int change=0;
+    private static int change = 0;
     private static List<String> altCode = new ArrayList<>();
     private static List<String> neuCode = new ArrayList<>();
     private static List<String> altTest = new ArrayList<>();
     private static List<String> neuTest = new ArrayList<>();
-    
+
     // Methoden newLines Code/Test gibt die Anzahl der Linien zurück, die neu hinzugefügt wurden
     public static List<Integer> newLinesCode(SourceCode alt, SourceCode neu) {
         split(alt, neu);
         List<Integer> lines = new ArrayList<>();
-        for (int i = 0; i<altCode.size(); i++) {
+        for (int i = 0; i < altCode.size(); i++) {
             String[] altArray = altCode.get(i).split("\n");
             String[] neuArray = neuCode.get(i).split("\n");
             lines.add(altArray.length - neuArray.length);
         }
         return lines;
     }
+
     public static List<Integer> newLinesTest(SourceCode alt, SourceCode neu) {
         split(alt, neu);
         List<Integer> lines = new ArrayList<>();
-        for (int i = 0; i<altTest.size(); i++) {
+        for (int i = 0; i < altTest.size(); i++) {
             String[] altArray = altTest.get(i).split("\n");
             String[] neuArray = neuTest.get(i).split("\n");
             lines.add(altArray.length - neuArray.length);
@@ -41,32 +39,15 @@ public class SourceCodeComparator {
 
     // Methoden changedLines Code/Test gibt die Anzahl der geänderten Zeilen zurück
     public static List<Integer> changedLinesCode(SourceCode alt, SourceCode neu) {
-       split(alt, neu);
+        split(alt, neu);
         List<Integer> lines = new ArrayList<>();
-        for (int i = 0; i<altCode.size(); i++) {
+        for (int i = 0; i < altCode.size(); i++) {
             String[] altArray = altCode.get(i).split("\n");
             String[] neuArray = neuCode.get(i).split("\n");
             List<String> tmp = new ArrayList<>();
             tmp = compare(altArray, neuArray);
-            if(altArray.length <= neuArray.length){
-                for (int j=altArray.length; j<neuArray.length; j++ ) {
-                    tmp.add(neuArray[j]);
-                }
-            }
-            lines.add(tmp.size());
-        }
-        return lines;
-    }
-    public static List<Integer> changedLinesTest (SourceCode alt, SourceCode neu) {
-        split(alt, neu);
-        List<Integer> lines = new ArrayList<>();
-        for (int i = 0; i<altTest.size(); i++) {
-            String[] altArray = altTest.get(i).split("\n");
-            String[] neuArray = neuTest.get(i).split("\n");
-            List<String> tmp = new ArrayList<>();
-            tmp = compare(altArray, neuArray);
-            if(altArray.length <= neuArray.length){
-                for (int j=altArray.length; j<neuArray.length; j++ ) {
+            if (altArray.length <= neuArray.length) {
+                for (int j = altArray.length; j < neuArray.length; j++) {
                     tmp.add(neuArray[j]);
                 }
             }
@@ -75,29 +56,52 @@ public class SourceCodeComparator {
         return lines;
     }
 
-    private static List<String> compare(String[] altArray, String[] neuArray){
+    public static List<Integer> changedLinesTest(SourceCode alt, SourceCode neu) {
+        split(alt, neu);
+        List<Integer> lines = new ArrayList<>();
+        for (int i = 0; i < altTest.size(); i++) {
+            String[] altArray = altTest.get(i).split("\n");
+            String[] neuArray = neuTest.get(i).split("\n");
+            List<String> tmp = new ArrayList<>();
+            tmp = compare(altArray, neuArray);
+            if (altArray.length <= neuArray.length) {
+                for (int j = altArray.length; j < neuArray.length; j++) {
+                    tmp.add(neuArray[j]);
+                }
+            }
+            lines.add(tmp.size());
+        }
+        return lines;
+    }
+
+    private static List<String> compare(String[] altArray, String[] neuArray) {
         List<String> diff = new ArrayList<>();
-        for(int i=0; i< altArray.length; i++) {
-            if(altArray[i].equals(neuArray[i]))
+        for (int i = 0; i < altArray.length; i++) {
+            if (altArray[i].equals(neuArray[i]))
                 continue;
             else {
-                diff.add(i+": "+neuArray[i]);
+                diff.add(i + ": " + neuArray[i]);
             }
         }
 //        change = diff.size();
         return diff;
     }
+
     private static void split(SourceCode alt, SourceCode neu) {
-        alt.getNameCode().forEach((a)-> {
+        altCode = new ArrayList<>();
+        altTest = new ArrayList<>();
+        neuCode = new ArrayList<>();
+        neuTest = new ArrayList<>();
+        alt.getNameCode().forEach((a) -> {
             altCode.add(alt.getStringCode(a));
         });
-        neu.getNameCode().forEach((n)-> {
+        neu.getNameCode().forEach((n) -> {
             neuCode.add(neu.getStringCode(n));
         });
-        alt.getNameTest().forEach((aT)-> {
+        alt.getNameTest().forEach((aT) -> {
             altTest.add(alt.getStringCode(aT));
         });
-        neu.getNameTest().forEach((t)-> {
+        neu.getNameTest().forEach((t) -> {
             neuTest.add(neu.getStringCode(t));
         });
     }
