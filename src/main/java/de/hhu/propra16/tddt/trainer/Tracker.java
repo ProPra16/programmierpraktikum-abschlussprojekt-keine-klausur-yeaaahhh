fc9c101633c88351c6ce4dd0ffc77cad2e09bfc9
Felix Michels <felixm.michels@gmail.com>
2016-07-13 18:44:11 +0200

    Some internal refactoring in Tracker

--- src/main/java/de/hhu/propra16/tddt/trainer/Tracker.java
+++ src/main/java/de/hhu/propra16/tddt/trainer/Tracker.java
@@ -5,6 +5,7 @@ import de.hhu.propra16.tddt.sourcecode.SourceCode;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Tracker {
 
@@ -14,13 +15,13 @@ public class Tracker {
         this.checker = checker;
     }
 
-    private static class TrackedObject {
+    private static class DataPoint {
         private final SourceCode code;
         private final Duration timeUsed;
         private final Phase oldPhase;
         private final Phase newPhase;
 
-        private TrackedObject(SourceCode code, Duration timeUsed, Phase oldPhase, Phase newPhase) {
+        private DataPoint(SourceCode code, Duration timeUsed, Phase oldPhase, Phase newPhase) {
             this.code = code;
             this.timeUsed = timeUsed;
             this.oldPhase = oldPhase;
@@ -28,17 +29,21 @@ public class Tracker {
         }
     }
 
-    private final List<TrackedObject> datapoints = new ArrayList<>();
+    private final List<DataPoint> datapoints = new ArrayList<>();
+
+    private List<DataPoint> phaseEnds() {
+        return datapoints.stream().filter(data -> data.oldPhase != data.newPhase).collect(Collectors.toList());
+    }
 
     public void push(SourceCode code, Duration timeUsed, Phase oldPhase, Phase newPhase) {
-        datapoints.add(new TrackedObject(code, timeUsed, oldPhase, newPhase));
+        datapoints.add(new DataPoint(code, timeUsed, oldPhase, newPhase));
     }
 
     public Duration getTotalTime(Phase phase) {
         Duration total = Duration.ZERO;
-        for (TrackedObject tracked : datapoints) {
-            if (tracked.oldPhase == phase && tracked.oldPhase != tracked.newPhase) {
-                total = total.plus(tracked.timeUsed);
+        for (DataPoint data : datapoints) {
+            if (data.oldPhase == phase && data.oldPhase != data.newPhase) {
+                total = total.plus(data.timeUsed);
             }
         }
         return total;
@@ -46,11 +51,11 @@ public class Tracker {
 
     public long numberOfFailedChecks(Phase phase) {
         return datapoints.stream().filter(tr -> tr.oldPhase == phase && tr.newPhase == phase)
-                .filter(tr -> !checker.check(tr.code, phase)).count();
+                .filter(data -> !checker.check(data.code, phase)).count();
     }
 
     public long numberOfCycles() {
-        return datapoints.stream().filter(tr -> tr.oldPhase == Phase.BLACK && tr.newPhase == Phase.RED)
+        return datapoints.stream().filter(data -> data.oldPhase == Phase.BLACK && data.newPhase == Phase.RED)
                 .count();
     }
 }
