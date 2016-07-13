f052456a797a60545b4315f8fe5f62e343c01960
Bennett <Bennett.K@gmx.de>
2016-07-13 14:59:44 +0200

    change Phase Enum to get next and previous Phase

--- src/main/java/de/hhu/propra16/tddt/trainer/Phase.java
+++ src/main/java/de/hhu/propra16/tddt/trainer/Phase.java
@@ -1,5 +1,25 @@
 package de.hhu.propra16.tddt.trainer;
 
 public enum Phase {
-    RED, GREEN, BLACK
+    RED, GREEN, BLACK;
+
+    private Phase next, previous;
+
+    static {
+        RED.next = GREEN;
+        GREEN.next = BLACK;
+        BLACK.next = RED;
+
+        RED.previous = BLACK;
+        GREEN.previous = RED;
+        BLACK.previous = GREEN;
+    }
+
+    public Phase next() {
+        return next;
+    }
+
+    public Phase previous() {
+        return previous;
+    }
 }
