0d883ae887da34a2a1b2703c23df608f08024ffa
Felix Michels <felixm.michels@gmail.com>
2016-07-13 21:02:37 +0200

    Add toString method in datapoint

--- src/main/java/de/hhu/propra16/tddt/trainer/DataPoint.java
+++ src/main/java/de/hhu/propra16/tddt/trainer/DataPoint.java
@@ -92,4 +92,23 @@ public class DataPoint {
     public int getLinesChanged() {
         return linesChanged;
     }
+
+    /**
+     * String representation of this datapoint
+     *
+     * @return String containing information of this datapoint
+     * if tabular form
+     */
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append(getPhase().name()).append('\t')
+                .append(getTimeUsed().getSeconds()).append('\t')
+                .append(getChecksWithFailedTests()).append('\t')
+                .append(getChecksWithCompilationError()).append('\t')
+                .append(getNewLinesOfCode()).append('\t')
+                .append(getNewLinesOfTest()).append('\t')
+                .append(getLinesChanged()).append('\t')
+                .toString();
+    }
 }
