cd53131e6e3a02a96d25f609660178571b46c4e5
Felix Michels <felixm.michels@gmail.com>
2016-07-13 20:48:07 +0200

    Modify inner class

--- src/main/java/de/hhu/propra16/tddt/trainer/Tracker.java
+++ src/main/java/de/hhu/propra16/tddt/trainer/Tracker.java
@@ -15,13 +15,13 @@ public class Tracker {
         this.checker = checker;
     }
 
-    private static class DataPoint {
+    private static class rawData {
         private final SourceCode code;
         private final Duration timeUsed;
         private final Phase phase;
         private final Phase newPhase;
 
-        private DataPoint(SourceCode code, Duration timeUsed, Phase phase, Phase newPhase) {
+        private rawData(SourceCode code, Duration timeUsed, Phase phase, Phase newPhase) {
             this.code = code;
             this.timeUsed = timeUsed;
             this.phase = phase;
@@ -29,14 +29,14 @@ public class Tracker {
         }
     }
 
-    private final List<DataPoint> datapoints = new ArrayList<>();
+    private final List<rawData> data = new ArrayList<>();
 
-    private List<DataPoint> phaseEnds() {
-        return datapoints.stream().filter(data -> data.phase != data.newPhase).collect(Collectors.toList());
+    private List<rawData> phaseEnds() {
+        return data.stream().filter(data -> data.phase != data.newPhase).collect(Collectors.toList());
     }
 
     public void push(SourceCode code, Duration timeUsed, Phase oldPhase, Phase newPhase) {
-        datapoints.add(new DataPoint(code, timeUsed, oldPhase, newPhase));
+        data.add(new rawData(code, timeUsed, oldPhase, newPhase));
     }
 
     public Duration getTotalTime(Phase phase) {
@@ -45,12 +45,12 @@ public class Tracker {
     }
 
     public long numberOfFailedChecks(Phase phase) {
-        return datapoints.stream().filter(data -> data.phase == phase && data.newPhase == phase)
+        return data.stream().filter(data -> data.phase == phase && data.newPhase == phase)
                 .filter(data -> !checker.check(data.code, phase)).count();
     }
 
     public long numberOfCycles() {
-        return datapoints.stream().filter(data -> data.phase == Phase.BLACK && data.newPhase == Phase.RED)
+        return data.stream().filter(data -> data.phase == Phase.BLACK && data.newPhase == Phase.RED)
                 .count();
     }
 
